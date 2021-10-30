ca86890f538c16411912d5c3623b8202e3710f3d
tanvirnpm <tanvirnpm@gmail.com>
2021-10-30 21:24:37 +0600

    service route added

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-const express = require('express')
-const { MongoClient } = require('mongodb');
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const port = 5000
-const uri = `mongodb+srv://sajek:${process.env.DB_PASS}@cluster0.2blon.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  client.connect(err => {
-    const servicesCollection = client.db(process.env.DB_NAME).collection("services");
-    
-  });
-})
-app.get('/add-service', (req, res) => {
-  client.connect(err => {
-    const servicesCollection = client.db(process.env.DB_NAME).collection("services");
-    
-  });
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2blon.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db(process.env.DB_NAME);
+    const serviceCollection = database.collection("services");
+
+    // insert service
+    app.post("/addService", async (req, res) => {
+      const receivedService = req.body;
+      const result = await serviceCollection.insertOne(receivedService);
+      res.json(result);
+    });
+
+    // get all service
+    app.get("/getService", async (req, res) => {
+      serviceCollection.find({}).toArray((err, documents) => {
+        res.send(documents);
+      });
+    });
+
+    // get single service details
+    app.post("/serviceDetails", async (req, res) => {
+      const receivedServiceId = req.body;
+      serviceCollection.find({_id: receivedServiceId}).toArray((err, documents) => {
+        res.send(documents);
+      });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Ema jon server is running");
+});
+
+app.listen(port, () => {
+  console.log("Server running at port", port);
+});
